f6cbf94f5cc1f9f70b93e92d7805edce34595a19
Tristan Hebert <91288854+tristanhebrt@users.noreply.github.com>
2022-10-01 21:25:23 -0400

    Implemented XML into JAVA. Declared a majority of the buttons, and added an onClick to display and clear the numbers on the calculator.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,95 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btn_one,
+    btn_two, btn_three, btn_four, btn_five,
+    btn_six, btn_seven, btn_eight, btn_nine,
+    btn_zero;
+
+    Button btn_plus,
+    btn_minus, btn_multiply, btn_divide, btn_dot,
+    btn_clear, btn_equal;
+
+    TextView text_display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_one = (Button) findViewById(R.id.btn_one);
+        btn_two = (Button) findViewById(R.id.btn_two);
+        btn_three = (Button) findViewById(R.id.btn_three);
+        btn_four = (Button) findViewById(R.id.btn_four);
+        btn_five = (Button) findViewById(R.id.btn_five);
+        btn_six = (Button) findViewById(R.id.btn_six);
+        btn_seven = (Button) findViewById(R.id.btn_seven);
+        btn_eight = (Button) findViewById(R.id.btn_eight);
+        btn_nine = (Button) findViewById(R.id.btn_nine);
+        btn_zero = (Button) findViewById(R.id.btn_zero);
+
+        btn_clear = (Button) findViewById(R.id.btn_clear);
+
+        text_display = (TextView) findViewById(R.id.textview_display);
+
+        setClickListeners();
+    }
+
+    private void setClickListeners(){
+        btn_one.setOnClickListener(this);
+        btn_two.setOnClickListener(this);
+        btn_clear.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btn_one:
+                addNumber("1");
+                break;
+            case R.id.btn_two:
+                addNumber("2");
+                break;
+            case R.id.btn_three:
+                addNumber("3");
+                break;
+            case R.id.btn_four:
+                addNumber("4");
+                break;
+            case R.id.btn_five:
+                addNumber("5");
+                break;
+            case R.id.btn_six:
+                addNumber("6");
+                break;
+            case R.id.btn_seven:
+                addNumber("7");
+                break;
+            case R.id.btn_eight:
+                addNumber("8");
+                break;
+            case R.id.btn_nine:
+                addNumber("9");
+                break;
+            case R.id.btn_zero:
+                addNumber("0");
+                break;
+            case R.id.btn_clear:
+                clear_display();
+                break;
+        }
+    }
+
+    private void addNumber(String number){
+        text_display.setText(text_display.getText()+number);
+    }
+
+    private void clear_display(){
+        text_display.setText("");
     }
 }
